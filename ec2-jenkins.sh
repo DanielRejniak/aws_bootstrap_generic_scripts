aac8ebb48279ad4c02f5b77d25877381c609df02
Daniel Rejniak <drejniak@ie.ibm.com>
2018-11-02 20:50:15 +0000

    Added new bootstrap script to setup jenkins server

---
+++ ec2-jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yum update -y
+yum install java -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install jenkins -y
+sudo service jenkins start
+chkconfig jenkins on
